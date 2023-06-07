d74c4b8cae81b56b526da1219c835c72c0c6dd46
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-06-08 00:25:10 +0600

    initial setup

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+};
+
+export default Main;

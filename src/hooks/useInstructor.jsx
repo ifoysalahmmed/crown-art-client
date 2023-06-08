07d17692e09071130a8c02d275d05a70fc470dc7
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-06-08 22:50:48 +0600

    added useInstructor hook

---
+++ src/hooks/useInstructor.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+const useInstructor = () => {
+  const { user, loading } = useAuth();
+
+  const [axiosSecure] = useAxiosSecure();
+
+  const { data: isInstructor, isLoading: isInstructorLoading } = useQuery({
+    queryKey: ["isAdmin", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/instructor/${user?.email}`);
+      return res.data.instructor;
+    },
+  });
+
+  return { isInstructor, isInstructorLoading };
+};
+
+export default useInstructor;
